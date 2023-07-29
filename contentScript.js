31591863c480b966248a24228dcd5f9ead4b8cf7
andres-condezo <ra.condezomonge@gmail.com>
2023-07-28 23:35:30 -0500

    [Feat][Create fetch function]

--- contentScript.js
+++ contentScript.js
@@ -12,9 +12,20 @@
         }
     });
 
-    const newVideoLoaded = () => {
+    const fetchBookmarks = () => {
+        return new fpromise((resolve)=>{
+            chrome.storage.sync.get([currentVideo], (obj) => {
+                resolve(obj[currentVideo] ? JSON.parse(obj[currentVideo]) : []);
+
+        })
+        })
+    }
+
+    const newVideoLoaded = async () => {
         const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
         console.log(bookmarkBtnExists);
+        currentVideoBookmarks = await fetchBookmarks();
+
 
         if (!bookmarkBtnExists) {
             const bookmarkBtn = document.createElement("img");
@@ -31,13 +42,16 @@
         }
     }
 
-    const addNewBookmarkEventHandler = () => {
+    const addNewBookmarkEventHandler = async () => {
         const currentTime = youtubePlayer.currentTime;
         const newBookmark = {
             time: currentTime,
             desc: "Bookmark at " + getTime(currentTime),
         };
         console.log(newBookmark);
+        currentVideoBookmarks = await fetchBookmarks();
+
+        currentVideoBookmarks
 
         chrome.storage.sync.set({
             [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
